75436fb2aa117afdc15cf0982692d18128a842b2
aryan malik <maddenn.f.l.2500@gmail.com>
2025-04-07 22:29:04 -0400

    arima

--- tools.py
+++ tools.py
@@ -2,12 +2,14 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import statsmodels.formula.api as smf
+import pmdarima as pm
+
 
 class Plotter:
     
     @staticmethod
-    def plot_prices_simple(df_prices, str_product):
-        df = df_prices[df_prices["product"] == str_product].copy()
+    def plot_prices_simple(df_data, str_product):
+        df = df_data[df_data["product"] == str_product].copy()
 
         df.index = pd.to_numeric(df.index)
         df = df.sort_index()
@@ -34,8 +36,8 @@ class Plotter:
         plt.show()
 
     @staticmethod
-    def plot_prices_full(df_prices, str_product):
-        df = df_prices[df_prices["product"] == str_product].copy()
+    def plot_prices_full(df_data, str_product):
+        df = df_data[df_data["product"] == str_product].copy()
 
         df.index = pd.to_numeric(df.index)
         df = df.sort_index()
@@ -65,13 +67,30 @@ class Plotter:
         plt.tight_layout()
         plt.show()
 
-class Regression:
+class Regressor:
 
     @staticmethod
-    def regress(df,x,y,element = "mid_price"):
-        X = df[df["product"]==x][element]
-        Y = df[df["product"]==y][element]
-        df = pd.DataFrame({"input" : X, "output" : Y})
-        model = smf.ols("output ~ input",data = df)
+    def regress_simple(df_data, X_product, Y_product, element = "mid_price"):
+        df = pd.DataFrame({
+            X_product: df_data[df_data["product"] == X_product][element],
+            Y_product: df_data[df_data["product"] == Y_product][element]
+        })
+        Regressor.linear_regression(df, [X_product], Y_product)
+
+    @staticmethod
+    def linear_regression(df_data, exog_labels: list, endog_label: str):
+        df = df_data[exog_labels + [endog_label]]
+        exog_string = ""
+        for label in exog_labels:
+            exog_string += f"+{label}"
+
+        model = smf.ols(f"{endog_label} ~ {exog_string[1:]}",data = df)
         results = model.fit()
         print(results.summary())
+
+    @staticmethod
+    def quick_arima(df_data, product, element = "mid_price", seasonal = False, stepwise = False, suppress_warnings = True):
+        df = df_data[df_data["product"] == product][element]
+        model = pm.auto_arima(df.values, seasonal = seasonal, stepwise = stepwise, suppress_warnings = suppress_warnings)
+
+        return model
